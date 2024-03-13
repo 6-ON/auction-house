f0829a20f5c379c823bcb3b4b859a367bbfbfc1c
6-ON <aminhatim6@gmail.com>
2024-03-13 22:00:09 +0000

    add featured and upcoming queries

--- auction-app/app/(main)/page.tsx
+++ auction-app/app/(main)/page.tsx
@@ -1,11 +1,15 @@
+import { getFeaturedAuctions } from '@/actions/featured-auctions'
 import { FeaturedAuctions, Footer, HeroSection, UpcomingAuctions } from '../../components/partial'
+import { getUpcomingAuctions } from '@/actions/upcoming-auctions'
 
 export default async function Home() {
+	const  featuredAuctions = await getFeaturedAuctions()
+	const upcomingAuctions = await getUpcomingAuctions()
 	return (
 		<main className="flex-1">
 			<HeroSection />
-			<FeaturedAuctions />
-			<UpcomingAuctions />
+			<FeaturedAuctions auctions={featuredAuctions} />
+			<UpcomingAuctions auctions={upcomingAuctions}/>
 			<Footer />
 		</main>
 	)
