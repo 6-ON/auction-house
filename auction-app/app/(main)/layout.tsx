5ad505a5de38146ad2c15e6fbde9abd63a9823a6
6-ON <aminhatim6@gmail.com>
2024-03-16 18:17:12 +0000

    Remove unused imports and variables in layout.tsx

--- auction-app/app/(main)/layout.tsx
+++ auction-app/app/(main)/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from 'next'
-import { Inter as FontSans } from 'next/font/google'
-import { cn } from '@/lib/utils'
 import { Nav } from '@/components/partial'
-import { SessionProvider } from 'next-auth/react'
 import { auth } from '@/lib/auth'
-export const fontSans = FontSans({
-	subsets: ['latin'],
-	variable: '--font-sans',
-})
+
 export const metadata: Metadata = {
 	title: 'Auction House',
 	description: 'a live auction platform for buying and selling goods.',
