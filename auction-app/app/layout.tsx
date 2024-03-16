872c0470ce084b4df3c8ed2c4180c8d5a1019692
6-ON <aminhatim6@gmail.com>
2024-03-16 20:52:19 +0000

    add darkmode support

--- auction-app/app/layout.tsx
+++ auction-app/app/layout.tsx
@@ -4,6 +4,7 @@ import './styles/globals.css'
 import { cn } from '@/lib/utils'
 import { ReduxProvider } from '@/components/redux/provider'
 import { Toaster } from '@/components/ui/toaster'
+import { ThemeProvider } from '@/components/providers/theme-provider'
 
 export const fontSans = FontSans({
 	subsets: ['latin'],
@@ -11,17 +12,21 @@ export const fontSans = FontSans({
 })
 export const metadata: Metadata = {
 	title: 'Auction House',
-	icons: { icon: '/logo.svg', },
+	icons: { icon: '/logo.svg' },
 	description: 'a live auction platform for buying and selling goods.',
 }
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<ReduxProvider>
-			<html lang="en">
-				<body className={cn('min-h-screen bg-background font-sans antialiased flex flex-col', fontSans.variable)}>
-					{children}
-					<Toaster />
+			<html lang="en" suppressHydrationWarning>
+				<body
+					className={cn('min-h-screen bg-background font-sans antialiased flex flex-col', fontSans.variable)}
+				>
+					<ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+						{children}
+						<Toaster />
+					</ThemeProvider>
 				</body>
 			</html>
 		</ReduxProvider>
