d456bc3ee84205f122a9b0088fa891732a26327d
6-ON <aminhatim6@gmail.com>
2024-03-17 19:50:13 +0000

    fix logic

--- auction-app/lib/auth.config.ts
+++ auction-app/lib/auth.config.ts
@@ -4,22 +4,18 @@ export const authConfig = {
 	providers: [],
 	callbacks: {
 		authorized({ auth, request: { nextUrl } }) {
-			console.log(nextUrl.pathname)
-
 			const isPublicRoute = /(^\/$|^\/auctions$)/.test(nextUrl.pathname)
 			const isAuthRoute = /(^\/sign-(in|up)$)/.test(nextUrl.pathname)
 			const isLoggedIn = !!auth?.user
-			// return isLoggedIn
 
 			// check if the route is public
 			if (isPublicRoute) return true
-
-			// check if user is logged in
-			if (!isLoggedIn) {
-				// authorize auth routes
-				if (isAuthRoute) return true
-				return false
-			} else if (isAuthRoute) return Response.redirect(new URL('/', nextUrl))
+			// if the user is logged in and the route is auth, redirect to the home page
+			if (isLoggedIn) {
+				if (isAuthRoute) return Response.redirect(new URL('/', nextUrl))
+				return true
+			}
+			return false
 		},
 	},
 	pages: {
