20d193335008471654985ae1d9c0064856d53d1e
6-ON <aminhatim6@gmail.com>
2024-03-24 16:45:30 +0000

    remove create auction when user banned

--- auction-app/components/common/user-dropdown.tsx
+++ auction-app/components/common/user-dropdown.tsx
@@ -1,6 +1,5 @@
 'use client'
-import { LogOut, PlusCircle, Settings, UserIcon } from 'lucide-react'
-import { Button } from '../ui/button'
+import { LogOut, PlusCircle, Settings, ShieldCheck, UserIcon } from 'lucide-react'
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -14,36 +13,41 @@ import { logout } from '@/actions/logout'
 import { User } from 'next-auth'
 import { Avatar, AvatarFallback } from '../ui/avatar'
 
-export function AccountDropdown({user}: {user: User}) {
+export function AccountDropdown({ user }: { user: User }) {
 	return (
 		<DropdownMenu>
-			<DropdownMenuTrigger className="p-1 hover:bg-black hover:bg-opacity-10 rounded-full">
+			<DropdownMenuTrigger className="p-1 hover:bg-muted hover:bg-opacity-10 rounded-full">
 				<Avatar>
-					<AvatarFallback>
-						{user.name?.[0]}
-					</AvatarFallback>
+					<AvatarFallback>{user.name?.[0]}</AvatarFallback>
 				</Avatar>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="me-4">
 				<DropdownMenuLabel>Hello {user.name} !</DropdownMenuLabel>
 				<DropdownMenuSeparator />
-				<DropdownMenuItem>
-					<Link href="/auctions/new" className="w-full flex gap-2 pe-12">
-						<PlusCircle className="w-5 h-5" />
-						<span>Create Auction</span>
-					</Link>
-				</DropdownMenuItem>
+				{!user.isBanned && (
+					<DropdownMenuItem>
+						<Link href="/auctions/new" className="w-full flex gap-2 pe-12">
+							<PlusCircle className="w-5 h-5" />
+							<span>Create Auction</span>
+						</Link>
+					</DropdownMenuItem>
+				)}
 				<DropdownMenuItem>
 					<Link href="/settings" className="w-full flex gap-2">
 						<Settings className="w-5 h-5" />
 						<span>Settings</span>
 					</Link>
 				</DropdownMenuItem>
+				{user.isAdmin && (
+					<DropdownMenuItem>
+						<Link href="/admin/dashboard" className="w-full flex gap-2">
+							<ShieldCheck className="w-5 h-5" />
+							<span>Adminstration</span>
+						</Link>
+					</DropdownMenuItem>
+				)}
 				<DropdownMenuSeparator />
-				<DropdownMenuItem
-					onClick={async () => await logout()}
-					className="hover:bg-red-500 hover:text-white gap-2 hover:cursor-pointer"
-				>
+				<DropdownMenuItem onClick={async () => await logout()} className="cursor-pointer flex gap-2">
 					<LogOut className="w-5 h-5" />
 					<span>Log out</span>
 				</DropdownMenuItem>
